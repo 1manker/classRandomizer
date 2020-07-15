f2d65eb2eaeaaaa75d9e940d0b95ad6019ffe176
1manker <lmanker@uwyo.edu>
2020-07-14 18:02:55 -0600

    load save works

--- TabWidget.py
+++ TabWidget.py
@@ -38,7 +38,7 @@ class TabWidget(QWidget):
 		buttonsLayout.addWidget(self.b1, 0, 0)
 		self.b1.clicked.connect(lambda:self.whichbtn(self.b1))
 
-		self.b2 = QPushButton('View Class List')
+		self.b2 = QPushButton('Save Class List')
 		buttonsLayout.addWidget(self.b2, 0, 1)
 		self.b2.clicked.connect(lambda:self.whichbtn(self.b2))
 
@@ -48,6 +48,7 @@ class TabWidget(QWidget):
 
 		#text box for class list
 		self.info = QTextBrowser(self.tab1)
+		self.info.setReadOnly(True)
 		self.info.setMinimumHeight(100)
 		self.info.setMaximumHeight(200)
 		buttonsLayout.addWidget(self.info, 1, 0, 3, 0)
@@ -58,16 +59,55 @@ class TabWidget(QWidget):
 
 	def whichbtn(self,b):
 		#figures out what button is clicked, then does stuff
-		print("clicked buttons is " + b.text())
 		if(b.text() == 'Upload Class List'):
-			self.getfiles()
+			self.getList()
+			self.info.setReadOnly(True)
+		if(b.text() == 'Edit Class List'):
+			self.info.setReadOnly(False)
+		if(b.text() == 'Save Class List'):
+			self.saveList()
+			self.info.setReadOnly(True)
 
-	def getfiles(self):
+	def getList(self):
+		#opens file explorer for list upload
 		options = QFileDialog.Options()
 		options |= QFileDialog.DontUseNativeDialog
 		fileName, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;", options=options)
 		if fileName:
 			print(fileName[0])
 			f = open(fileName[0], 'r')
+			lines = f.readlines()
+			#check for formatting
+			for line in lines:
+				split = line.split(',')
+				if(len(split) != 4):
+					#ask user if they still want to load bad file
+					msg = QMessageBox()
+					msg.setIcon(QMessageBox.Critical)
+					msg.setText("List Improperly Formatted")
+					msg.setInformativeText("Load Anyway?")
+					msg.setWindowTitle("Error")
+					msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+					ret = msg.exec()
+					if(ret == QMessageBox.Cancel):
+						self.info.setText('')
+						f.close()
+						return()
+			#go back to beginning of file
+			f.seek(0,0)
+			#put the file contents in the textbox
 			self.info.setText(f.read())
+			f.close()
+
+	def saveList(self):
+		#file explorer to save list
+		options = QFileDialog.Options()
+		options |= QFileDialog.DontUseNativeDialog
+		fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()", "","All Files (*);;", options=options)
+		if fileName:
+			print(fileName)
+			f = open(fileName, 'w')
+			#convert contents of textbox to plain text and write to file
+			text = self.info.toPlainText()
+			f.write(text)
 			f.close()
